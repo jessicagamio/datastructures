5039b9c3ce53f64e5d849354cf809e756b55f397
jessicagamio <jessica.gamio@gmail.com>
2019-12-02 15:49:05 -0800

    completed validateBST function

--- bst.py
+++ bst.py
@@ -7,3 +7,25 @@ class BST(object):
         self.data = data
         self.left = None
         self.right = None
+
+
+    def validateBST(self, node, min = None, max = None):
+        """Returns True if a Binary Search Tree"""
+
+        if node == None:
+            return True
+
+        # add base case that breaks the function call if node doesn't comply with BST rules
+        if min != None and node.data <= min or max != None and node.data > max:
+            return False
+
+        # call functions to each sub tree
+        if node.left:
+            validateBST(node.left,None,node.data)
+
+        if node.right:
+            validateBST(node.right,node.data,None)
+
+        return True
+
+    def checkBalance()
